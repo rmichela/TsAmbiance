7069c55d2d59a32c335abe56733dd84571c7c66f
rmichela <deltahat@gmail.com>
2012-06-25 16:37:06 -0400

    Added world configuration

--- src/main/java/com/ryanmichela/toxicskies/ambiance/AmbiancePlugin.java
+++ src/main/java/com/ryanmichela/toxicskies/ambiance/AmbiancePlugin.java
@@ -1,6 +1,7 @@
 package com.ryanmichela.toxicskies.ambiance;
 
 import org.bukkit.World;
+import org.bukkit.configuration.Configuration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AmbiancePlugin extends JavaPlugin {
@@ -8,11 +9,17 @@ public class AmbiancePlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        World world = getServer().getWorld("world");
-        world.setStorm(true);
-        world.setWeatherDuration(200);
-        world.setThundering(true);
-        world.setThunderDuration(200);
+        saveDefaultConfig();
+
+        Configuration config = getConfig();
+        for (String worldName : config.getStringList("AffectedWorlds")) {
+            getLogger().info("Adding ambiance to " + worldName);
+            World world = getServer().getWorld(worldName);
+            world.setStorm(true);
+            world.setWeatherDuration(200);
+            world.setThundering(true);
+            world.setThunderDuration(200);
+        }
     }
 
     @Override
